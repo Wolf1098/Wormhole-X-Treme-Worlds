37be6e3097764ab2bb3f0e8f2e158f458b737c08
Wolf1098 <Wolf1098@wolfietech.xyz>
2022-04-04 08:07:39 -0700

    Strap gradle buildroot for new dev enviroment

---
+++ src/main/java/xyz/wolfietech/dev/minecraft/paper/wormhole/xtreme/worlds/remake/Paper/wormhole/xtreme/worlds/remake.java
@@ -0,0 +1,17 @@
+package xyz.wolfietech.dev.minecraft.paper.wormhole.xtreme.worlds.remake.Paper.wormhole.xtreme.worlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class remake extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
